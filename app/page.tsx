4a06c8070c4820f67415a6e230784ed25ecb6a7e
Orkhan Rahimli <orduxan144@gmail.com>
2023-07-14 23:14:53 +0500

    fix use client

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,14 @@
 import styles from './page.module.css'
 import News from './components/News'
 import { INews } from './dashboard/page'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-  const news = JSON.parse(window.localStorage.getItem('news') || '')
+  const [news, setNews] = useState<INews[]>([])
+
+  useEffect(() => {
+    setNews(JSON.parse(window.localStorage.getItem('news') || ''))
+  }, [])
 
   return (
     <main className={styles.wrapper}>
