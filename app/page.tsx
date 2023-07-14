6f68b46e1d5b41b4a58a9c01439f39b6c901bd20
Orkhan Rahimli <orduxan144@gmail.com>
2023-07-14 23:34:54 +0500

    fix localstorage

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,14 @@
 import styles from './page.module.css'
 import News from './components/News'
 import { INews } from './dashboard/page'
-import { useEffect, useState } from 'react'
+import useLocalStorage from './hooks/useLocalStorage'
 
 export default function Home() {
-  const [news, setNews] = useState<INews[]>([])
-
-  JSON.parse(window.localStorage.getItem('news') || '')
+  const [news, setNews] = useLocalStorage('news', '')
 
   return (
     <main className={styles.wrapper}>
-      {news.map((item: INews) => (
-        <News key={item.id} news={item} />
-      ))}
+      {news && news.map((item: INews) => <News key={item.id} news={item} />)}
     </main>
   )
 }
